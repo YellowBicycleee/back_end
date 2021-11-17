ba23847e075946e3a647b4469b6f76899b18f8e2
jiqirui <1085436942@qq.com>
2021-11-17 16:00:19 +0800

    添加后端项目模板

---
+++ backend/src/main/java/com/rg/backend/util/Constant.java
@@ -0,0 +1,7 @@
+package com.rg.backend.util;
+
+public class Constant {
+    public static final String PRODUCE_ORIGIN = "http://47.94.129.13:80";
+    public static final String DEVELOP_ORIGIN = "http://localhost:8080";
+    public static final String VERSION = "v0";
+}
